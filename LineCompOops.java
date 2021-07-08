c1f4ffa0be489a8c714f4c6d6b350b88b526bcb9
yashpurwar1 <yashpurwar.yashpurwar.com>
2021-07-08 16:04:55 +0530

    Added: Welcome message

---
+++ LineCompOops.java
@@ -0,0 +1,10 @@
+package lineComparisionOops;
+
+public class LineCompOops {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to line comparision computation prblm");
+		
+	}
+
+}
