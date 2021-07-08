8815d894f13d33c02a9b5c5a0448ec339445fdb7
yashpurwar1 <yashpurwar.yashpurwar.com>
2021-07-08 16:38:45 +0530

    Added: Fuctionality to check weather two lines are equal or not

--- LineCompOops.java
+++ LineCompOops.java
@@ -3,9 +3,20 @@ package lineComparisionOops;
 import java.util.Scanner;
 
 public class LineCompOops {
-	public static double lengthCalc(double x1, double y1, double x2, double y2) {
+	private static double lengthCalc(double x1, double y1, double x2, double y2) {
 		double length = Math.sqrt(((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1)));
 		return length;
+	}
+	/*it will check for equality of two lines*/
+	@SuppressWarnings("removal")
+	private static int lengthEquality(double length, double length1) {
+		int flag=0;
+		Double len = new Double(length);
+		Double len1 = new Double(length1);
+		if (len.equals(len1)) {
+			flag = 1;
+		}
+		return flag;
 	}
 	
 	public static void main(String[] args) {
@@ -17,7 +28,16 @@ public class LineCompOops {
 		double x2 = scanner.nextDouble();
 		double y2 = scanner.nextDouble();
 		double length = lengthCalc(x1, y1, x2, y2);
+		System.out.println("Enter a1, b1 and a2, b2 for line 2");
+		double a1 = scanner.nextDouble();
+		double b1 = scanner.nextDouble();
+		double a2 = scanner.nextDouble();
+		double b2 = scanner.nextDouble();
+		double length1 = lengthCalc(a1, b1, a2, b2);
+		//if flag equals 1 then line are equal otherwise not equal
+		int flag = lengthEquality(length, length1);
 		scanner.close();
 	}
+
 
 }
