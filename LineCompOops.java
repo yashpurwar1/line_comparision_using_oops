a696715c3397691e677e4be6e0e709d26187e793
yashpurwar1 <yashpurwar.yashpurwar.com>
2021-07-08 16:18:50 +0530

    Added: Functionality to calculate the length of line

--- LineCompOops.java
+++ LineCompOops.java
@@ -1,10 +1,23 @@
 package lineComparisionOops;
 
-public class LineCompOops {
+import java.util.Scanner;
 
+public class LineCompOops {
+	public static double lengthCalc(double x1, double y1, double x2, double y2) {
+		double length = Math.sqrt(((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1)));
+		return length;
+	}
+	
 	public static void main(String[] args) {
 		System.out.println("Welcome to line comparision computation prblm");
-		
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter x1, y1 and x2, y2");
+		double x1 = scanner.nextDouble();
+		double y1 = scanner.nextDouble();
+		double x2 = scanner.nextDouble();
+		double y2 = scanner.nextDouble();
+		double length = lengthCalc(x1, y1, x2, y2);
+		scanner.close();
 	}
 
 }
